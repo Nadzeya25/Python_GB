de350f81957b7868d171a3bd130e9164889161af
Nadzeya <lustra2013@gmail.com>
2022-12-18 19:34:17 +0300

    add correct .\task1.2.py

--- seminar1_Python/Home_work1/task1.2.py
+++ seminar1_Python/Home_work1/task1.2.py
@@ -1,11 +1,3 @@
-#6. Пример проверки ложности утверждения (x ≡ z ) ∨ (x → (y ∧ z)) 
-#print("x y z") 
-#for x in range(2): 
-  #  for y in range(2): 
-   #     for z in range(2): 
-    #        if not (x == z or x <= y and z): 
-     #           print(x, y, z) 
- 
 #2. Напишите программу для проверки ложности утверждения 
 #(W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат. 
 print("x y z w") 
@@ -15,3 +7,15 @@ for x in range(2):
             for w in range(2): 
                 if not ((w and z) or not y or ((not x) == (not w))): 
                     print( x, y, z, w) 
+
+# Задача 2. Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
+# для всех значений предикат.
+
+# for x in [False, True]:
+#     for y in [False, True]:
+#         for z in [False, True]:
+#             if (not x or y or z) != (not x or y or z):
+#                 print('Выражение: ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - False')
+#                 break
+# else:
+#     print('Выражение: ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - True')
