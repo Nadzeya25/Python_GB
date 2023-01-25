2e7de38cb2e979e09a15a3c54d65baa21d173779
Nadzeya <lustra2013@gmail.com>
2023-01-25 17:42:56 +0300

    добавлены изм

--- seminar5_Python/home_work5_tester/task_t_5_1.py
+++ seminar5_Python/home_work5_tester/task_t_5_1.py
@@ -59,4 +59,15 @@ f3 = filtr_list(f2)
 print(" ".join(f2))    
 print(" ".join(f3))
 
+# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+#  Решение из парраллельной группы
+# def main():
+#     txt = 'ansабвdfk afdhiаБвauh asdhfia fisadpof adfuабвh afai iqaqfqif afhiaf fad'
+#     print(txt)
+#     txt = ' '.join(list(filter(lambda element: 'абв' not in element.lower(), txt.split())))
+#     print(txt)
+# if __name__ == "__main__":
+#     main()
+
+
 
